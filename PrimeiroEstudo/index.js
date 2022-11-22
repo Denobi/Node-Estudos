da89fa534530087fa47ada0e9a1bd82d71431056
Guilherme Denobi <you@example.com>
2022-11-22 09:22:44 -0300

    summary

---
+++ PrimeiroEstudo/index.js
@@ -0,0 +1,7 @@
+const {Person} = require("./person");
+
+
+require('./modules/path')
+// const person = new Person('Guilherme');
+
+// console.log(person.sayMyName())
---
+++ PrimeiroEstudo/modules/path.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+// BASE NAME
+//  APENAS O NOME DO ARQUIVO
+
+console.log(path.basename(__filename));
+//Nome do diretorio atual
+console.log(path.dirname (__filename));
